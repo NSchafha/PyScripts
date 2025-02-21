bde2809999a74640476d8d146e97ac8fcce7cb4e
Nicholaus <njwschafhauser@gmail.com>
2025-02-21 08:02:16 -0500

    dotW

--- AnyDotW.py/README.md
+++ AnyDotW.py/README.md
@@ -1,7 +1,9 @@
-# Algorithm to tell you the day of the week for any date
+# Day of the Week Algo
+Simple calculation algorithm to tell you the day of the week for any date after 1799
 
 ## Implementation
-
+The implementation of this algorithm is based on Zellers congruence outlined in: 
+https://en.wikipedia.org/wiki/Zeller%27s_congruence#:~:text=Zeller
 
 ## Use
 python(3) dotW.py \[month\] \[day\] \[year\]
--- AnyDotW.py/dotW.py
+++ AnyDotW.py/dotW.py
@@ -6,3 +6,23 @@ parser.add_argument("day")
 parser.add_argument("year")
 args = parser.parse_args()
 
+months = {
+    "Jan" : 13, "Feb" : 14, "Mar" : 3, "Apr" : 4, "May" : 5, "Jun" : 6, 
+    "Jul" : 7, "Aug" : 8, "Sep" : 9, "Oct" : 10, "Nov" : 11, "Dec" : 12
+}
+
+day = int(args.day)
+
+month = months[args.month]
+monthPart = 13 * (month + 1) // 5
+
+year = int(args.year)
+year = year - 1 if month > 12 else year
+yearPart = year + (year // 4) - (year // 100) + (year // 400)
+
+dotW = int((day + monthPart + yearPart) % 7)
+
+weekdays = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+
+print(f"Day of the week for {args.month} {day}, {args.year} is: {weekdays[dotW]}")
