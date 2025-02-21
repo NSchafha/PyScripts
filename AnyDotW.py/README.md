ae419b1fbe78ac3761367a67ff9b76fe542ecfc4
Nicholaus <njwschafhauser@gmail.com>
2025-02-20 22:38:37 -0500

    dotW

---
+++ AnyDotW.py/README.md
@@ -0,0 +1,8 @@
+# Algorithm to tell you the day of the week for any date
+
+## Implementation
+
+
+## Use
+python(3) dotW.py \[month\] \[day\] \[year\]
+
---
+++ AnyDotW.py/dotW.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("month")
+parser.add_argument("day")
+parser.add_argument("year")
+args = parser.parse_args()
+
